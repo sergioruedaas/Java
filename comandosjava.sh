f3c587d0c7b820d6ae6823c5ff881bb88d4f7304
Sergio Rueda <sergioruedaas@gmail.com>
2023-12-04 17:54:15 +0100

    primer commit

---
+++ comandosjava.sh
@@ -0,0 +1,10 @@
+git init		#iniciar repo
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git config --global --list		#listar la primera configuracion del git
+
+git add .		#añadir archivo al staging area
+git commit -m "ejercicio1.java"		#crear commit
+git log 		#lista los commit
+git status
+
